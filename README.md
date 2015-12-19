c588751dd1ca6e2a3c6f640fff544c061447c41c
WILLIAM KING <WKINGCREATIVE@GMAIL.COM>
2015-12-19 00:37:42 -0600

    changed the directory structure to provide access directly to the css files containing the buttons. moved all files used in the sample page into the dist folder and the files prepaed to create a bower package into the package folder.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 This is just something I made during a span of projects where I wanted to use elements from Google's Material Design without having to pull in an entire framework like Angular Material or Materialize
 
 All you need to do is copy the style.min.css into your project and use the classes shown in index.html
-Future Plan:
+
+## ToDo:
 I will be adding better docs soon!
---
+++ dist/gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			sass: {
+				files: ['stylesheets/*.scss'],
+				tasks: ['sass', 'autoprefixer', 'cssmin']
+			}
+		},
+
+		autoprefixer: {
+            dist: {
+                files: {
+                    'css/style.css': 'css/style.css'
+                }
+            }
+        },
+
+		sass : {
+			dist: {
+				files: {
+					'css/style.css': 'stylesheets/style.scss'
+				}
+			}
+		},
+
+		concat: {
+			options: {
+				separator: ";",
+				stripBanners: true,
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
+			},
+			dist: {
+				src: ['js/placeholder.js', 'js/placeholder2/js'],
+				dest: 'js/main.min.js'
+			}
+		},
+
+		uglify: {
+			options: {
+				manage: false
+			},
+			my_target: {
+				files: {
+					'js/main.min.js': ['js/main.js'],
+					'js/modernizr.min.js': ['js/modernizr.js'],
+					'js/jquery-2.1.1.min.js': ['js/jquery-2.1.1.js']
+				}
+			}
+		},
+
+		cssmin: {
+			my_target: {
+				files: [{
+					expand: true,
+					cwd: 'css/',
+					src:['style.css', 'reset.css', '!*.min.css'],
+					dest: 'css/',
+					ext: '.min.css'
+				}]
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['watch']);
+};
